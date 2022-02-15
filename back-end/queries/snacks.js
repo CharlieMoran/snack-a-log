3c93f7d84721133505739f03cba876cec7b03d67
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-15 12:16:24 -0500

    getSnackAll querie 1

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -1,3 +1,16 @@
 const db = require("../db/dbConfig.js");
 
-module.exports = {};
+const getSnacksAll = async () => {
+  try {
+    let allSnacks = await db.any("SELECT * FROM snacks");
+    return allSnacks;
+  } catch (error) {
+    return error;
+  }
+};
+
+
+
+module.exports = {
+  getSnacksAll,
+};
