e937a8d1de3f9dc1fa10ab48f755311d08df4f67
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-15 11:58:46 -0500

    Schema set up

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -1,6 +1,13 @@
 DROP DATABASE IF EXISTS snack_a_log;
-CREATE DATABASE snack_a_log; 
+CREATE DATABASE snack_a_log;
 
-\c snack_a_log; 
+\c snack_a_log;
 
-
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY, 
+    name TEXT, 
+    fiber INTEGER, 
+    protein INTEGER, 
+    added_sugar INTEGER, 
+    is_healthy BOOLEAN Default false, 
+    image TEXT);
