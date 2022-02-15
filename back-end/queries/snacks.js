c9d29af77752363e6aeb40b0d71f4fc64651c208
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-15 12:24:10 -0500

    createSnack querie 3

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -18,11 +18,24 @@ const getSnack = async (id) => {
     }
   };
   
+  const createSnack = async (snack) => {
+    let { name, fiber, protein, added_sugar, is_healthy, image } = snack;
+    try {
+      let newSnack = await db.one(
+        "INSERT INTO snacks (name, fiber, protein, added_sugar, is_healthy, image) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ",
+        [name, fiber, protein, added_sugar, is_healthy, image]
+      );
+      return newSnack;
+    } catch (error) {
+      return error;
+    }
+  };
   
-
+  
 
 
 module.exports = {
   getSnacksAll,
   getSnack,
+  createSnack,
 };
