9312dd70d10c23aa6426ee0df9e2b7a28fc4d83c
Charlie Moran <Carlosamoran18@gmail.com>
2022-02-15 12:50:45 -0500

    snacks get

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { getSnack, createSnack, getSnack, deleteSnack, updateSnack} = require('../queries/snacks');
+const { getSnacksAll, createSnack, getSnack, deleteSnack, updateSnack } = require('../queries/snacks');
 const snacks = express.Router();
+
+snacks.get('/', async (req, res) => {
+    let snacks = await getSnacksAll();
+    console.log(snacks)
+    res.status(200).json(snacks)
+});
 
 module.exports = snacks;
